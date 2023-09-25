36359a6f17b006132c4345887dd4e5b4fa3a4d48
Jeeyoon Shim <missed.ult@gmail.com>
2023-09-25 15:30:01 -0400

    Added Logout.js, updated the NavBar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { getAuth, onAuthStateChanged, signOut } from './firebaseConfig';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Register from './Register';
+import Balance from './Balance';
+import Login from './Login';
+import Logout from './Logout';
+import Home from './Home';
+import Deposit from './Deposit';
+import Withdraw from './Withdraw';
+import NavBarLoggedIn from './NavBarLoggedIn';
+import NavBarLoggedOut from './NavBarLoggedOut';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, user => {
+      if (user) {
+        setUser(user);
+        console.log(`Current logged in user: ${user.email}`) // check who is logged in
+      } else {
+        setUser(null);
+        console.log(`Current logged in user: null`)
+      }
+    });
+  }, []);
+
+  const handleLogout = async () => {
+    const auth = getAuth();
+    try {
+      await signOut(auth);
+      setUser(null); 
+    } catch (error) {
+      console.error('Logout failed:', error.message);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {user ? <NavBarLoggedIn /> : <NavBarLoggedOut />}
+      <Router>
+        <Routes>
+          {user ? (
+            <>
+              <Route path="/" element={<Home />} />
+              <Route path="/Register/" element={<Register />} />
+              <Route path="/Logout/" element={<Logout onLogout={handleLogout} />} />
+              <Route path="/Balance/" element={<Balance uid={user.uid} />} />
+              <Route path="/Deposit/" element={<Deposit uid={user.uid} />} />
+              <Route path="/Withdraw/" element={<Withdraw uid={user.uid} />} />
+            </>
+          ) : (
+            <>
+              <Route path="/" element={<Home />} />
+              <Route path="/Register/" element={<Register />} />
+              <Route path="/Login/" element={<Login />} />
+            </>
+          )}
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Balance.js
@@ -0,0 +1,47 @@
+import { React, useState } from "react";
+import { readUserData } from "./firebaseDatabase";
+
+function Balance({ uid }){
+    const [show, setShow] = useState(true);
+    const [status, setStatus] = useState('');
+    const [balance, setBalance] = useState('');
+  
+
+    const handleSubmit = async e => {
+        // const userRef = db.collection('users').doc('peter@mit.edu');
+        // const doc = await userRef.get();
+        // if (!doc.exists) {
+        //   console.log('No such document!');
+        // } else {
+        //   console.log('Document data:', doc.data());
+        // }
+        console.log('button is clicked');
+        const userDetails = await readUserData(uid);
+        console.log(userDetails);
+        console.log(userDetails.balance)
+        setStatus(`Your balance is: ${userDetails.balance}`);
+      };
+  
+      return (
+        <>
+          <h2>Balance</h2>
+          <form>
+            <div className="form-group">
+              <label>Check Balance:</label>
+            </div>
+            <button type="button" className="btn btn-primary" onClick={handleSubmit}>
+              Check
+            </button>
+            <p>
+                {status}
+            </p>
+          </form>
+        </>
+      );
+    }
+  
+
+
+  
+
+  export default Balance;
